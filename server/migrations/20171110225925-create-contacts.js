6ea1ce11e6f27e167e5baa527a82751d9a987b66
mlamont8 <mlamont8@hotmail.com>
2017-11-19 21:16:36 -0500

    setup first test

--- server/migrations/20171110225925-create-contacts.js
+++ server/migrations/20171110225925-create-contacts.js
@@ -31,6 +31,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Contacts');
+    return queryInterface.dropTable('contacts');
   }
 };
--- src/components/gridContainer/gridContainer.test.js
+++ src/components/gridContainer/gridContainer.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import GridContainer from './gridContainer';
 
+
+// Grid Container Smoke test
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<GridContainer />, div);
+  shallow(<GridContainer />)
 });
